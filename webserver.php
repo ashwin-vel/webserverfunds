7df98e5db5838ecae2c10eb2b57601ce271f8ab2
ashwin-vel <ashwin_vel@hotmail.com>
2024-10-02 15:13:12 -0400

    finAL21

--- webserver.php
+++ webserver.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
     <body>
-        <div> Hi, your name is: <?= "Hello, " . $_POST['firstname']  . " " . $_POST['lastname'] .""; ?>. 
+        <div> Hi, your name is: <?= "Hello, " . htmlspecialchars($_POST['firstname']  . " " . $_POST['lastname'] ."") ?>. 
         <br>
-        Your School is: <?= $_POST['school'] ?>. 
+        Your School is: <?= htmlspecialchars($_POST['school']) ?>. 
         <br>
-        Your Birthday: <?= $_POST['birthday'] ?>. 
+        Your Birthday: <?= htmlspecialchars($_POST['birthday']) ?>. 
     </div>
     </body>
  </html>
