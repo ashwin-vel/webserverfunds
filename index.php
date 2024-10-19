f0c5998aa18339b0902af7e374c99b6a785daf8d
ashwin-vel <ashwin_vel@hotmail.com>
2024-10-19 18:18:23 -0400

    dfd

--- index.php
+++ index.php
@@ -111,11 +111,14 @@
             <input type="submit" value="submit"/>  
         </form>
         <form action="webserver.php" method="get">
-            <label for="custo">Enter Customer Name:</label><br/>
+            <label for="custo">Enter Customer Name:</label>
+            <br/>
             <input type="text"  name="custo"> 
-            <label for="sinnum1">Enter Sin Num:</label><br/>
+            <label for="sinnum1">Enter Sin Num:</label>
+            <br/>
             <input type="text"  name="sinnum1"> 
-            <label for="banknam">Enter Bank Name:</label><br/>
+            <label for="banknam">Enter Bank Name:</label>
+            <br/>
             <input type="text"  name="banknam"> 
             <br/>
             <input type="submit" value="submit"/>  
